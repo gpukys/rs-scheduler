5d971ec1aba39c5979b245b534bbe26c3eed7329
Gerimantas Pukys <gerimantas_pukys@epam.com>
2022-04-06 11:38:27 +0300

    try pm2 once again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,16 +11,24 @@ module.exports = {
       script: 'dist/server.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instances: 1, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
+      // autorestart: true, // auto restart if process crash
+      // watch: false, // files change automatic restart
+      // ignore_watch: ['node_modules', 'logs'], // ignore files change
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: {
         // environment variable
         NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_staging: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
       },
     },
     {
