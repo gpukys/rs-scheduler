1610f96308f9086c1ffbc0a31851a8b98cc4bbde
Gerimantas Pukys <gerimantas_pukys@epam.com>
2022-07-13 18:37:45 +0300

    change the deadlines

--- ui/src/app/scheduler/scheduler.component.ts
+++ ui/src/app/scheduler/scheduler.component.ts
@@ -71,7 +71,7 @@ export class SchedulerComponent implements OnInit {
   }
 
   cantSeeNextWeek(): boolean {
-    return isAfter(add(this.today, {days: 7}), new Date('2022-04-29'));
+    return isAfter(add(this.today, {days: 7}), new Date('2022-08-01'));
   }
 
   handleEventClick(event: any) {
