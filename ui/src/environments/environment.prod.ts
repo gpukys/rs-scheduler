b66d6b551344df0bfa102c4153959cd4538f0a9f
Gerimantas Pukys <gerimantas_pukys@epam.com>
2022-11-17 23:30:05 +0200

    damn it

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { Environment } from "./environment.base";
 export const environment: Environment = {
   production: true,
   apiRoot: 'https://rs-scheduler.onrender.com/api/',
-  discordLoginUrl: 'https://discord.com/api/oauth2/authorize?client_id=1042909972792610826&redirect_uri=https%3A%2F%2Frs-scheduler.onrender.com%2F&response_type=code&scope=identify%20guilds%20guilds.members.read'
+  discordLoginUrl: 'https://discord.com/api/oauth2/authorize?client_id=1042909972792610826&redirect_uri=https%3A%2F%2Frs-scheduler.onrender.com&response_type=code&scope=identify%20guilds%20guilds.members.read'
 };
